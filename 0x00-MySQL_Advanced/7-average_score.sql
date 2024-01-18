85b528266cdbae400bf71ba5dc4ad9e20fbb22d0
joelsteven <joelofelectronics@gmail.com>
2024-01-19 02:34:50 +0300

    Fix:task_7 query

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,23 +1,13 @@
 -- Creates a stored procedure that computes and stores the average score for a student
-
-DELIMITER $$
-
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id_param INT)
+DROP procedure IF EXISTS ComputeAverageScoreForUser;
+DELIMITER |
+CREATE PROCEDURE ComputeAverageScoreForUser (
+		IN user_id INT
+		)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-
-    SET total_score = 0;
-    SET total_projects = 0;
-
-    FOR each_correction IN (SELECT score FROM corrections WHERE user_id = user_id_param) DO
-        SET total_score = total_score + each_correction.score;
-        SET total_projects = total_projects + 1;
-    END FOR;
-
-    IF total_projects > 0 THEN
-        UPDATE users SET average_score = total_score / total_projects WHERE id = user_id_param;
-    END IF;
-END $$
-
-DELIMITER ;
+UPDATE users
+  SET average_score=(SELECT AVG(score) FROM corrections
+		    WHERE corrections.user_id=user_id)
+WHERE id=user_id;
+END;
+|

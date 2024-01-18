99108e34ce2edac9b8d770be5a1510f00f6efc87
joelsteven <joelofelectronics@gmail.com>
2024-01-19 01:37:09 +0300

    Add:task_10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,18 @@
+--SQL script that creates a function SafeDiv
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL(10, 4)
+BEGIN
+    DECLARE result DECIMAL(10, 4);
+
+    IF b = 0 THEN
+        SET result = 0;
+    ELSE
+        SET result = a / b;
+    END IF;
+
+    RETURN result;
+END //
+
+DELIMITER ;

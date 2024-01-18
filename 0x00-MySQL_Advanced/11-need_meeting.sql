a0475b6965e8a24c172b139f8a85cc79867d9f3f
joelsteven <joelofelectronics@gmail.com>
2024-01-19 02:26:42 +0300

    Fix:task-11 query

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,4 +4,4 @@
 CREATE VIEW need_meeting AS
 SELECT name
 FROM students WHERE score < 80 
-ADD (last_meeting IS NULL OR last_meeting < DATE(CURDATE(), INTERVAL 1 MONTH));
+ADD (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));

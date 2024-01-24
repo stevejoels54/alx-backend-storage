b7e32a68c3ef94110edc3ab4404f2f118c234cf5
joelsteven <joelofelectronics@gmail.com>
2024-01-24 05:25:56 +0300

    Add top student

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Sorts students by average score (Top students)
+"""
+
+
+def top_students(mongo_collection):
+    """ returns sorted students by average score """
+
+    pipeline = [
+        {
+            "$project": {
+                "name": $name,
+                "averageScore": {
+                    "$avg": "$topics.score"
+                }
+            }
+        },
+        {
+            "$sort": {"averageScore": -1}
+        }
+    ]
+
+    result = list(mongo_collection.aggregate(pipeline))
+    return result

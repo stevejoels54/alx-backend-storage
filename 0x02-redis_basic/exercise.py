087149582b2f3229acff34bd15d08865e775276d
joelsteven <joelofelectronics@gmail.com>
2024-01-25 04:19:37 +0300

    Add read from redis recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Redis basic.
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,19 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str, fn: Optional[Callable] = None) -> str:
+        """Takes key str arg, convert data to desired format"""
+        data = self._redis.get(key)
+        if data is not None and fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, data: str) -> str:
+        """Returns string value of decoded byte"""
+        return data.decode('utf-8', 'strict')
+
+    def get_int(self, data: str) -> int:
+        """Returns integer value of decoded byte """
+        return int(data)
